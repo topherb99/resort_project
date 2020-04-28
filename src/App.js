459b480b9dba357f549f0876999256d8bcb71eb3
Topher <topherb99@gmail.com>
2020-04-28 10:58:19 +0100

    added paths for 3 pages

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      Hello from App page
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/rooms/" component={Rooms}/>
+      <Route exact path="/singleroom/" component={SingleRoom}/>
     </>
   );
 }
