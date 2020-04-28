f02f19d91c799b7478389f546521f011abaec7fe
Topher <topherb99@gmail.com>
2020-04-28 12:17:05 +0100

    added link to homepage + navbar icon

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/rooms/" component={Rooms}/>
-      <Route exact path="/rooms/:params" component={SingleRoom}/>
-      <Route component={Error}/>
-    </Switch>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms}/>
+        <Route exact path="/rooms/:params" component={SingleRoom}/>
+        <Route component={Error}/>
+      </Switch>
     </>
   );
 }
