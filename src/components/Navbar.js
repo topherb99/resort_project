dccbbb9b46bb1b5592ae9a20acd49d97b69abd34
Topher <topherb99@gmail.com>
2020-04-28 12:50:09 +0100

    added: nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,10 +21,22 @@ Component {
             <Link to="/">
               <img src={logo} alt="Topher's Paradise Palms"/>
             </Link>
-            <button type="button" className="nav-btn">
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
               <FaAlignRight className="nav-icon"/>
             </button>
           </div>
+          <ul className={this.state.isOpen?"nav-links show-navs":"nav-links"}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     )
