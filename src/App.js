91eb9cf5ec503515120d4a15742a87a88ffdf8b6
Topher <topherb99@gmail.com>
2020-04-28 10:06:57 +0100

    added: new pages (rooms and error)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React from 'react';
 
 function App() {
   return (
-    <>Hello there
+    <>
+      Hello from App page
     </>
   );
 }
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      Hello from Error page
+    </div>
+  )
+}
+
+export default Error
