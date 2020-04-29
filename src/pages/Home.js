522f7f9b2f65aeb4ca423eb12278fa8e097ed50a
Topher <topherb99@gmail.com>
2020-04-29 11:21:07 +0100

    banner added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
-  return <Hero></Hero>;
+  return <Hero>
+
+  <Banner title="Amazing rooms" subtitle="Chips and gravy delivered to your bed">
+    <Link to="/rooms" className="btn-primary">
+      our rooms
+    </Link>
+  </Banner>
+
+  </Hero>;
 }
