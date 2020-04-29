2672be33a542230864884e530f88ea750dfb3a51
Topher <topherb99@gmail.com>
2020-04-29 10:40:40 +0100

    hero component added to all pages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Error = () => {
   return (
-    <div>
-      Hello from Error page
-    </div>
+    <Hero></Hero>
   )
 }
 
