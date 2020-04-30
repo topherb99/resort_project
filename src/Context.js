944828021cd0254cbcae7b37ec181d1a0e05117f
Topher <topherb99@gmail.com>
2020-04-30 12:53:08 +0100

    index wrapped with RoomProvider

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
-const RoomContext = React.createContext
+const RoomContext = React.createContext();
 
-export default class RoomProvider extends Component {
+class RoomProvider extends Component {
+  state = {};
   render () {
-    return <div />;
+    return (
+      <RoomContext.Provider value="hello">
+        {this.props.children}
+      </RoomContext.Provider>
+    );
   }
 }
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext }
