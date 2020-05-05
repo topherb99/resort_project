5c8f33e0e8136824986e66e1fc474ec9dd335368
Topher <topherb99@gmail.com>
2020-05-05 11:14:45 +0100

    added: getRoom function + imports to singleRoom page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../Context'
 
-export default class SingleRoom extends
-Component {
+export default class SingleRoom extends Component {
   render() {
     return (
       <div>
