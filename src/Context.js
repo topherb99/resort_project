4f2f06d793b92fc1b97b458fed802e45211830f6
Topher <topherb99@gmail.com>
2020-05-05 11:18:04 +0100

    spelling error

--- src/Context.js
+++ src/Context.js
@@ -36,7 +36,7 @@ class RoomProvider extends Component {
 
   getRoom = (slug) =>{
     let tempRooms = [...this.state.rooms];
-    xonst room = tempRooms.find(room => room.slug === slug);
+    const room = tempRooms.find(room => room.slug === slug);
     return room;
   }
 
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -6,6 +6,13 @@ import {Link} from 'react-router-dom'
 import {RoomContext} from '../Context'
 
 export default class SingleRoom extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.props)
+  }
+  componentDidMount(){
+
+  }
   render() {
     return (
       <div>
