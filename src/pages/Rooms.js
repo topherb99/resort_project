b9f3979699d27df25cfbd649113f947b011ef51e
Topher <topherb99@gmail.com>
2020-05-06 11:36:58 +0100

    filter, list and container showing on rooms page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import RoomsContainer from '../components/RoomsContainer'
 
 const Rooms = () => {
   return (
-    <Hero hero="roomsHero">
-      <Banner title="Rooms">
-        <Link to="/" className="btn-primary">
-          Back home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="Rooms">
+          <Link to="/" className="btn-primary">
+            Back home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer/>
+    </>
   )
 }
 
